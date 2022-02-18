4b49070be63099067070aaf777ee46ea2ffeb868
Devin <dolsen1@pkglobal.com>
2022-02-18 00:16:45 -0800

    1.4.6-patch: Adds duplicate filtering for both symbols and manifest file

--- test/test.a.half.js
+++
@@ -1,4 +0,0 @@
-// Adding images to bundle for WebpackSVGSpritely to chew on
-require('./images/up.svg');
-require('./images/down.svg');
-require('../assets/logo.png');
--- test/test.b.half.js
+++
@@ -1,4 +0,0 @@
-// Adding images to bundle for WebpackSVGSpritely to chew on
-require('./images/left.svg');
-require('./images/right.svg');
-require('../assets/logo.png');
